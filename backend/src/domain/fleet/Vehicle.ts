30c305592ef5001f74f39e03135f927e7f9add5f
scaraude <ludodante@hotmail.fr>
2026-02-08 18:50:26 +0100

    feature: create Vahicule entity

---
+++ backend/src/domain/fleet/Vehicle.ts
@@ -0,0 +1,19 @@
+import type { Location } from "../shared/Location.js";
+
+export class Vehicle {
+  private _location?: Location;
+
+  constructor(public readonly plateNumber: string) { }
+
+  get location(): Location | undefined {
+    return this._location;
+  }
+
+  park(location: Location): void {
+    this._location = location;
+  }
+
+  isParkedAt(location: Location): boolean {
+    return this._location?.equals(location) ?? false;
+  }
+}
