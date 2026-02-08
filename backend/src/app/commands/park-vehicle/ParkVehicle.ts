757bfbe59e6ab2f2e31ab0257f9b40d3002b49a4
scaraude <ludodante@hotmail.fr>
2026-02-08 18:53:07 +0100

    add parkVahicule command

---
+++ backend/src/app/commands/park-vehicle/ParkVehicle.ts
@@ -0,0 +1,6 @@
+export interface ParkVehicle {
+  fleetId: string;
+  vehiclePlateNumber: string;
+  latitude: number;
+  longitude: number;
+}
---
+++ backend/src/app/commands/park-vehicle/ParkVehicleHandler.ts
@@ -0,0 +1,19 @@
+import { FleetNotFoundError } from "../../../domain/fleet/errors.js";
+import type { FleetRepository } from "../../../domain/fleet/FleetRepository.js";
+import { Location } from "../../../domain/shared/Location.js";
+import type { ParkVehicle } from "./ParkVehicle.js";
+
+export class ParkVehicleHandler {
+  constructor(private fleetRepository: FleetRepository) { }
+
+  async handle(command: ParkVehicle): Promise<void> {
+    const fleet = await this.fleetRepository.findById(command.fleetId);
+    if (!fleet) {
+      throw new FleetNotFoundError(command.fleetId);
+    }
+
+    const location = new Location(command.latitude, command.longitude);
+    fleet.parkVehicle(command.vehiclePlateNumber, location);
+    await this.fleetRepository.save(fleet);
+  }
+}
